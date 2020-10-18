032d1d38e31e9d30a5163db9dde6303f5b8ff953
jtkaczyszyn <jtkaczyszyn@elementum.com>
2020-10-18 19:17:16 -0300

    add protected page stub

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
 } from 'react-router-dom';
 
 import Profile from './Profile';
@@ -42,6 +45,61 @@ const Main = () => {
   );
 }
 
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function ProtectedPage() {
+  return <h3>Protected</h3>;
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
+}
+
 const App = () => {
   return (
     <Router>
@@ -54,10 +112,19 @@ const App = () => {
             <li>
               <Link to="/profile">Profile</Link>
             </li>
+            <li>
+              <Link to="/protected">Protected Page</Link>
+            </li>
           </ul>
         </nav>
 
         <Switch>
+          <PrivateRoute path="/protected">
+            <ProtectedPage />
+          </PrivateRoute>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
           <Route path="/profile">
             <Profile />
           </Route>
